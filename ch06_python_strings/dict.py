4141fbef3f4a6ab63028020a02eaef6f6ccf9609
Fazal Abbas <newthought492@gmail.com>
2026-05-10 17:53:17 +0500

    dictionary added

---
+++ ch06_python_strings/dict.py
@@ -0,0 +1,24 @@
+# Dictionaries
+# Dictionaries are used to store values in key values paris.
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+print(thisdict)
+print(thisdict["brand"])
+print(thisdict["model"])
+print(thisdict["year"])
+# Length of dictionary
+print(len(thisdict))
+# Dictionary items can be of any data type, and they can be mixed.
+dict_1 = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964,
+"colors": ["red", "white", "blue"],
+"electric": False,
+"price": 30000.00
+}
+print(dict_1)
+print(len(dict_1))
