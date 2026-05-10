c2781035474e33defa1e9b078c5953d2ce5be49c
Fazal Abbas <newthought492@gmail.com>
2026-05-10 18:43:59 +0500

    if_else concepts added

---
+++ ch06_python_strings/if_else.py
@@ -0,0 +1,18 @@
+# Python If.............Else
+# Python supports usual logical conditions from mathematics.
+# Equals:                   a==b
+# Not Equals:               a!=b
+# Less than:                a<b
+# Less than or equal to:    a<=b
+# Greater than:             a>b
+# Greater than or equal to: a>=b
+# These conditions can be used in several ways.Most commonly in "if statements" and "loops".
+# An if statement is written by using the word "if".
+a = 65
+b = 55
+if a>b:
+  print("a is greater than b")
+elif a==b:
+  print("a is equal to b")
+
+
