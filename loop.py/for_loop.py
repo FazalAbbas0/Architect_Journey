d996d20bf9c15b76bc130a57cf81571a000a0f16
Fazal Abbas <newthought492@gmail.com>
2026-05-11 18:47:36 +0500

    For_Loop Added

---
+++ loop.py/for_loop.py
@@ -0,0 +1,5 @@
+inventory = ["4_Ton_Sugar", "1_K_Liters_Water", "1K_Bottles_Rooh_Afza", "2_K_Block_Ice"]
+print("---[SABEEL STARTED FOR MAJLIS]----")
+for item in inventory:
+    print(f"{item} donated for Majlis.")
+print("----[SABEEL PREPARED FOR MAJLIS]----")
